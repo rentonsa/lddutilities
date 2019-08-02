ca2b055569209d4ce15b39c1b709ea24692fcd39
rentonsa <rentonsa@gmail.com>
2019-08-02 16:19:25 +0100

    Change query- it does not require a date or be on test

--- python/luna_from_vernon/luna_from_vernon_variables.py
+++ python/luna_from_vernon/luna_from_vernon_variables.py
@@ -4,10 +4,9 @@ dspace loader variables
 """
 ALL_VARS = {
     'MAP_FILE' : 'mapping.txt',
-    'API_URL' : 'http://test.vernonapi.is.ed.ac.uk/vcms-api/oecgi4.exe/datafiles/AV/?query=',
+    'API_URL' : 'http://vernonapi.is.ed.ac.uk/vcms-api/oecgi4.exe/datafiles/AV/?query=',
     'FIELDS' : '&fields=id,im_ref,caption,brief_desc,prod_pri_person,publication_status,credit_line,user_text_2,user_sym_1,user_sym_2,user_sym_3,user_sym_4,user_sym_5,user_sym_6,user_sym_7,user_sym_8,user_sym_9,user_sym_10,user_sym_11,user_sym_12,user_sym_13,user_sym_14,user_sym_15,user_sym_15,user_sym_17,user_sym_18,user_sym_19,user_sym_20',
     'LIMIT' : '&limit=10000',
-    'API_AV_CHECK_URL': 'http://test.vernonapi.is.ed.ac.uk/vcms-api/oecgi4.exe/datafiles/AV?query=',
     'ART_QUERY': 'squery:"ArtNotyetIIIF")',
     'MIMED_QUERY' : 'squery:"MIMEdNotyetIIIF")'
 }
